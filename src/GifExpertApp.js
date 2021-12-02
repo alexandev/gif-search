795aa3282d99e74af33140dcfc75e0fed0c0622f
Alexander Moreno <alexander.morenoc@outlook.com>
2021-12-02 09:54:46 -0500

    fetch api

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,13 +1,10 @@
 import React, {useState} from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GiftGrid';
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState( ['One Punch', 'Samurai X', 'Dragon Ball'] )
-
-  // const handleAdd = () => {
-  //   setCategories(cats => [...cats, 'HunterXHunter']);
-  // }
+  const [categories, setCategories] = useState( ['One Punch'] )
 
   return(
     <>
@@ -17,9 +14,12 @@ export const GifExpertApp = () => {
 
       <ol>
         {
-          categories.map(category => {
-            return <li key = {category}> {category} </li>
-          })
+          categories.map(category => (
+            <GifGrid 
+            key = { category }
+            category={ category }
+            />
+          ))
         }
       </ol>
     </>
